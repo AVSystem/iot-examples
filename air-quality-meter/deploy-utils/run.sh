97876fd11847a2c7f7a48af5c8e1e4c78bd7aec1
s.seget <s.seget@avsystem.com>
2021-03-18 17:26:06 +0100

    Changes and improvements

--- air-quality-meter/deploy-utils/run.sh
+++ air-quality-meter/deploy-utils/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Djava.library.path=library/build/cmake -jar air-quality-meter/build/libs/air-quality-meter.jar --randomize=true --lifetime 90 -e $DEVICEID -u coap://$SERVER_ADDRESS:5683
+java -Djava.library.path=. -jar ./air-quality-meter.jar --randomize --lifetime 90 -e $DEVICEID -u coap://$SERVER_ADDRESS:5683
