8f8bffc86a743df8f4ff99d6627b4d8a476d88cf
Jakub Pinowski <j.pinowski@avsystem.com>
2021-10-19 11:41:24 +0200

    [IDI-509] cloudFormation and terraform roles adjusted

--- coiote-aws-iot-terraform/main.tf
+++ coiote-aws-iot-terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+  name = "iam_for_lambda_3"
 
   assume_role_policy = <<EOF
 {
@@ -15,6 +15,22 @@ resource "aws_iam_role" "iam_for_lambda" {
   ]
 }
 EOF
+
+  inline_policy {
+    name = "SecretsManagerForLambdaPolicy"
+
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "GetSecrets",
+          "Action": "secretsmanager:GetSecretValue",
+          "Resource": "*",
+          "Effect": "Allow"
+        }
+      ]
+    })
+  }
 }
 
 resource "aws_iam_policy_attachment" "policy_attachment" {
@@ -45,18 +61,11 @@ resource "aws_lambda_function" "lwm2mOperation" {
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   runtime = "python3.8"
-  environment {
-    variables = {
-      coioteDMrestPassword = var.coioteDMrestPassword
-      coioteDMrestUri = var.coioteDMrestURL
-      coioteDMrestUsername = var.coioteDMrestUsername
-    }
-  }
 
 }
 
 module "iot_rule_1" {
-  name = "operationRequest"
+  name = "operationRequest_3"
   sql_query = "SELECT state.desired.operation AS operation, state.desired.keys AS keys, state.desired.values AS values, state.desired.attributes AS attributes, state.desired.arguments AS arguments, topic(3) AS thingName FROM '$aws/things/+/shadow/name/operation/update/accepted' WHERE isUndefined(state.desired.operation) = false"
   source  = "git::https://github.com/Passarinho4/terraform-aws-iot-topic-rule.git?ref=master" 
   lambda = ["lwm2mOperation"]
@@ -64,7 +73,7 @@ module "iot_rule_1" {
 }
 
 module "iot_rule_2" {
-  name = "operationResponse"
+  name = "operationResponse_3"
   sql_query = "SELECT state.reported.result AS state.reported FROM '$aws/things/+/shadow/name/operation/update/accepted' WHERE (CASE isUndefined(state.reported.operation) WHEN true THEN false ELSE CASE state.reported.operation = 'read' OR state.reported.operation = 'write' OR state.reported.operation = 'readComposite' when true THEN true ELSE false END END) = true"
   source = "git::https://github.com/Passarinho4/terraform-aws-iot-topic-rule.git?ref=master"
   republish = [{topic = "$$aws/things/$${topic(3)}/shadow/name/datamodel/update"}]
