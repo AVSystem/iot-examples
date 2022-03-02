03fe9b3bce8a1567e1ac65d077cbf40618060d28
ptekielak <p.tekielak@avsystem.com>
2022-03-02 13:35:35 +0100

    Review fixes

--- coiote-aws-iot-cloud-formation/createThingType/create_thing_type.py
+++ coiote-aws-iot-cloud-formation/createThingType/create_thing_type.py
@@ -2,10 +2,12 @@ import boto3
 
 client = boto3.client('iot')
 
+# coioteDeviceType is for example anjay.demo_client
+def extract_device_type(event):
+    return event['coioteDeviceType'].replace('.', ':')
 
 def lambda_handler(event, context):
-    # coioteDeviceType is for example anjay.demo_client
-    thing_type = event['coioteDeviceType'].replace('.', ':')
+    thing_type = extract_device_type(event)
     existing_thing_types = client.list_thing_types()['thingTypes']
     existing_thing_types_names = [thing['thingTypeName'] for thing in existing_thing_types]
     if thing_type not in existing_thing_types_names:
