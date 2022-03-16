062163af65cd0935a7af571d846fe775b30b16b3
ptekielak <p.tekielak@avsystem.com>
2022-03-16 14:54:29 +0100

    CDM-455

--- coiote-aws-iot-cloud-formation/createThing/create_thing.py
+++ coiote-aws-iot-cloud-formation/createThing/create_thing.py
@@ -1,15 +1,27 @@
 import boto3
+import json
 
-client = boto3.client('iot')
+iot_client = boto3.client('iot')
+iot_data_client = boto3.client('iot-data')
+
+
+def ensure_thing_type_exists(thing_type):
+    existing_thing_types = iot_client.list_thing_types()['thingTypes']
+    existing_thing_types_names = [thing['thingTypeName'] for thing in existing_thing_types]
+    if thing_type not in existing_thing_types_names:
+        iot_client.create_thing_type(thingTypeName=thing_type)
 
 
 def lambda_handler(event, context):
     thing_name = event['coioteDeviceId']
     thing_type_name = event['coioteDeviceType']
+    ensure_thing_type_exists(thing_type_name)
     attribute_payload = {
         'attributes': {
             'protocol': 'LwM2M'
         },
         'merge': True
     }
-    client.create_thing(thingName=thing_name, thingTypeName=thing_type_name, attributePayload=attribute_payload)
+    iot_client.create_thing(thingName=thing_name, thingTypeName=thing_type_name, attributePayload=attribute_payload)
+    payload = json.dumps({"state": {}}).encode('utf-8')
+    iot_data_client.update_thing_shadow(thingName=thing_name, shadowName='operation', payload=payload)
